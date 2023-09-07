768aff89dffc91c3335a583af89bbd82fccee979
Bagabandi <76275588+Bagabandi152@users.noreply.github.com>
2023-09-07 11:16:02 +0800

    update

---
+++ src/main/java/mn/sync/hikvisionbrige/models/Department.java
@@ -0,0 +1,8 @@
+package mn.sync.hikvisionbrige.models;/**
+*@author Bagaa
+*@project hikvision-brige
+*@created 07/09/2023 - 9:10 AM
+*@purpose 
+*@definition 
+*/public class Department {
+}
---
+++ src/main/java/mn/sync/hikvisionbrige/models/SyncEmpData.java
@@ -0,0 +1,8 @@
+package mn.sync.hikvisionbrige.models;/**
+*@author Bagaa
+*@project hikvision-brige
+*@created 07/09/2023 - 9:59 AM
+*@purpose 
+*@definition 
+*/public class SyncEmpData {
+}
